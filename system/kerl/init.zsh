e1761e900f411b575d252521a7418c4a7b9ec90a
goose <purification1997@gmail.com>
2025-02-19 21:01:55 +0700

    Update erlang installation path

--- system/kerl/init.zsh
+++ system/kerl/init.zsh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ -d "$HOME/.kerl/27.2.1-builtdocs" ] && zsh-defer source "$HOME/.kerl/27.2.1-builtdocs/activate"
+[ -d "$HOME/.kerl/27.2.1" ] && zsh-defer source "$HOME/.kerl/27.2.1/activate"
