fada624494ffba822f9745f6c6e339d5c3f2104d
goose <purification1997@gmail.com>
2025-03-28 21:08:14 +0700

    Add no-stream opts for aider

--- zsh/aider.zsh
+++ zsh/aider.zsh
@@ -1,1 +1,1 @@
-alias aider='ANTHROPIC_API_KEY=$(op read "op://Private/Anthropic API key/API key" --no-newline) aider --watch-files --cache-prompts --editor "nvim -u $HOME/.config/nvim/init_lite.lua" --dark-mode  --no-attribute-author --no-attribute-committer'
+alias aider='ANTHROPIC_API_KEY=$(op read "op://Private/Anthropic API key/API key" --no-newline) aider --watch-files --cache-prompts --editor "nvim -u $HOME/.config/nvim/init_lite.lua" --dark-mode  --no-attribute-author --no-attribute-committer --no-stream'
