8ddb9cdae5e62255fe10055414b0c99e04d03e81
goose <purification1997@gmail.com>
2025-03-28 12:51:18 +0700

    Better editor

--- zsh/aider.zsh
+++ zsh/aider.zsh
@@ -1,1 +1,1 @@
-alias aider='ANTHROPIC_API_KEY=$(op read "op://Private/Anthropic API key/API key" --no-newline) aider --watch-files --cache-prompts'
+alias aider='ANTHROPIC_API_KEY=$(op read "op://Private/Anthropic API key/API key" --no-newline) aider --watch-files --cache-prompts --editor "nvim -u $HOME/.config/nvim/init_lite.lua" --dark-mode'
