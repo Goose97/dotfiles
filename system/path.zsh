e8d3b0048be9a2691cf7de9217610e1ff156421d
goose <purification1997@gmail.com>
2025-12-13 11:00:23 +0700

    add pub cache to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.pub-cache/bin:$PATH"
